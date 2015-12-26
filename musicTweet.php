4d9ad86f3ec361df67f63821f25f4795f09c5fe8
Keita Yamada <yshiro9@yahoo.co.jp>
2015-12-27 03:25:52 +0900

    phpcsw

--- musicTweet.php
+++ musicTweet.php
@@ -1,24 +1,21 @@
 <?php
-require_once("./youtube_apis/search_youtube.php");
-require_once("./music/tweetHandler.php");
+
+require_once './youtube_apis/search_youtube.php';
+require_once './music/tweetHandler.php';
 
 # メンションを拾う
 $th = new tweetHandler();
-$mentions=$th->getMentions();
+$mentions = $th->getMentions();
 
 $sm = new searchMusic();
 
 # 名前
-foreach($mentions as $mention){
+foreach ($mentions as $mention) {
+    $url = $sm->search($mention['music_name']);
 
-$url=$sm->search($mention["music_name"]);
+    $dtext = $mention['music_name'].' '.$url;
+    $dreply_id = $mention['reply_id'];
+    $dscreen_name = $mention['screen_name'];
 
-$dtext = $mention["music_name"]." ".$url;
-$dreply_id = $mention["reply_id"];
-$dscreen_name = $mention["screen_name"];
-
-$th->post($dtext,$dreply_id,$dscreen_name);
-
+    $th->post($dtext, $dreply_id, $dscreen_name);
 }
-
-?>
